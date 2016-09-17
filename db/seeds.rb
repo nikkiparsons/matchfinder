51421c1118b3d7f0eb9579aa4d70934c37e7a6bf
Nikki Parsons <nikki.a.parsons@gmail.com>
2016-09-17 12:30:46 +0200

    Bars page shows the ul of matches it is showing

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,78 +11,77 @@ arsSouthMatch = Match.create!(
   home_team: "Arsenal",
   away_team: "Southampton",
   league: "Premier League",
-  match_time: DateTime.strptime("10/09/2016 13:00", "%m/%d/%Y %H:%M")
-  #this should have a bars list
+  match_time: DateTime.strptime("10/09/2016 13:00", "%m/%d/%Y %H:%M"),
 )
 
-Match.create!(
+bournWestMatch = Match.create!(
   home_team: "Bournemouth",
   away_team: "West Brom",
   league: "Premier League",
-    match_time: DateTime.strptime("10/09/2016 14:00", "%m/%d/%Y %H:%M")
+  match_time: DateTime.strptime("10/09/2016 14:00", "%m/%d/%Y %H:%M")
 )
 
-Match.create!(
+burnHullMatch = Match.create!(
   home_team: "Burnley",
   away_team: "Hull City",
   league: "Premier League",
-    match_time: DateTime.strptime("10/09/2016 14:00", "%m/%d/%Y %H:%M")
+  match_time: DateTime.strptime("10/09/2016 14:00", "%m/%d/%Y %H:%M")
 )
 
-Match.create!(
+middleCrystelMatch = Match.create!(
   home_team: "Middlesbrough",
   away_team: "Crystal Palace",
   league: "Premier League",
-    match_time: DateTime.strptime("10/09/2016 13:00", "%m/%d/%Y %H:%M")
+  match_time: DateTime.strptime("10/09/2016 13:00", "%m/%d/%Y %H:%M")
 )
 
-Match.create!(
+stokeSpursMatch = Match.create!(
   home_team: "Stoke",
   away_team: "Tottenham",
   league: "Premier League",
-    match_time: DateTime.strptime("10/09/2016 16:00", "%m/%d/%Y %H:%M")
+  match_time: DateTime.strptime("10/09/2016 16:00", "%m/%d/%Y %H:%M")
 )
 
-Match.create!(
+hammWatfMatch = Match.create!(
   home_team: "West Ham",
   away_team: "Watford",
   league: "Premier League",
-    match_time: DateTime.strptime("10/09/2016 16:00", "%m/%d/%Y %H:%M")
+  match_time: DateTime.strptime("10/09/2016 16:00", "%m/%d/%Y %H:%M")
 )
 
-Match.create!(
+liverLeiceMatch = Match.create!(
   home_team: "Liverpool",
   away_team: "Leicester",
   league: "Premier League",
-    match_time: DateTime.strptime("10/09/2016 18:00", "%m/%d/%Y %H:%M")
+  match_time: DateTime.strptime("10/09/2016 18:00", "%m/%d/%Y %H:%M")
 )
 
-Match.create!(
+vigoAtlMatch = Match.create!(
   home_team: "Celta Vigo",
   away_team: "Atletico Madrid",
   league: "La Liga",
-    match_time: DateTime.strptime("10/09/2016 18:00", "%m/%d/%Y %H:%M")
+  match_time: DateTime.strptime("10/09/2016 18:00", "%m/%d/%Y %H:%M")
 )
 
-Match.create!(
+rmOsasMatch = Match.create!(
   home_team: "Real Madrid",
   away_team: "Osasuna",
   league: "La Liga",
-    match_time: DateTime.strptime("10/09/2016 20:00", "%m/%d/%Y %H:%M")
+  match_time: DateTime.strptime("10/09/2016 20:00", "%m/%d/%Y %H:%M")
 )
 
-Match.create!(
+sevilPalmMatch = Match.create!(
   home_team: "Sevilla",
   away_team: "Las Palmas",
   league: "La Liga",
-    match_time: DateTime.strptime("10/09/2016 20:00", "%m/%d/%Y %H:%M")
+  match_time: DateTime.strptime("10/09/2016 20:00", "%m/%d/%Y %H:%M")
 )
 
-Match.create!(
+utdCityMatch = Match.create!(
   home_team: "Manchester Utd",
   away_team: "Manchester City",
   league: "Premier League",
-    match_time: DateTime.strptime("10/09/2016 18:00", "%m/%d/%Y %H:%M")
+  match_time: DateTime.strptime("10/09/2016 18:00", "%m/%d/%Y %H:%M")
 )
 
 #BARS
@@ -90,19 +89,22 @@ Match.create!(
 georgePayneBar = Bar.create!(
   bar_name: "The George Payne",
   bar_address: "Plaça d'Urquinaona 5, 08010, Barcelona",
-  website: "http://www.thegeorgepayne.com/"
+  website: "http://www.thegeorgepayne.com/",
+  matches: [arsSouthMatch]
 )
 
 Bar.create!(
   bar_name: "Flaherty's Irish Bar",
   bar_address: "Plaza Joaquim Xirau, 08002, Barcelona",
-  website: "http://www.pflaherty.com/barcelona-home/"
+  website: "http://www.pflaherty.com/barcelona-home/",
+  matches: [utdCityMatch, arsSouthMatch]
 )
 
 Bar.create!(
   bar_name: "Belushi's Bar",
   bar_address: "Carrer de Bergara 3, 08002, Barcelona",
-  website: "http://www.belushis.com/bars/barcelona"
+  website: "http://www.belushis.com/bars/barcelona",
+  matches: [stokeSpursMatch, vigoAtlMatch, rmOsasMatch]
 )
 
-# georgePayneBar.matches.create(match: arsSouthMatch)
+# georgePayneBar.matches.create(match: match)
