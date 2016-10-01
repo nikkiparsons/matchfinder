ae5ccafc58aa4e560b5ff008716e8322f836dd92
Nikki Parsons <nikki.a.parsons@gmail.com>
2016-10-01 20:53:24 +0200

    Linked calendar matches to full match detail

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :bars
   resources :matches
   get 'homepage' => 'homepage#index'
-  get 'home' => 'home#index'
+  get '' => 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
