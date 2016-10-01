9be37d6b46f0d533d1f2ef5794eeaa072daaa955
Nikki Parsons <nikki.a.parsons@gmail.com>
2016-10-01 23:10:35 +0200

    Removed additional homepage and fixed all links to new home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :bars
   resources :matches
-  get 'homepage' => 'homepage#index'
   get '' => 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
