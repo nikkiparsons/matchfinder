c4b97d5ab3aa22a75f39a476bc9f98fc0b59dd93
Nikki Parsons <nikki.a.parsons@gmail.com>
2016-10-05 21:51:11 +0200

    Created contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :bars
   resources :matches
   get '' => 'home#index'
+  get '/contact' => 'contact#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
